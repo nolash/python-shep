78a3df73bbf175805d950bd28a455c9e4085a82b
lash <dev@holbrook.no>
2022-01-31 08:38:14 +0000

    Complete base and alias inserts

--- schiz/error.py
+++ schiz/error.py
@@ -1,2 +1,6 @@
 class StateExists(Exception):
     pass
+
+
+class StateInvalid(Exception):
+    pass
--- tests/test_state.py
+++ tests/test_state.py
@@ -3,7 +3,10 @@ import unittest
 
 # local imports
 from schiz import State
-from schiz.error import StateExists
+from schiz.error import (
+        StateExists,
+        StateInvalid,
+        )
 
 
 class TestState(unittest.TestCase):
@@ -29,6 +32,36 @@ class TestState(unittest.TestCase):
         states.add('foo')
         with self.assertRaises(StateExists):
             states.add('foo')
+
+
+    def test_alias(self):
+        states = State(2)
+        states.add('foo')
+        states.add('bar')
+        states.alias('baz', states.FOO | states.BAR)
+        self.assertEqual(states.BAZ, 3)
+
+
+    def test_alias_limit(self):
+        states = State(2)
+        states.add('foo')
+        states.add('bar')
+        states.alias('baz', states.FOO | states.BAR)
+
+
+    def test_alias_nopure(self):
+        states = State(3)
+        with self.assertRaises(ValueError):
+            states.alias('foo', 4)
+
+
+    def test_alias_cover(self):
+        states = State(3)
+        states.add('foo')
+        states.add('bar')
+        with self.assertRaises(StateInvalid):
+            states.alias('baz', 5)
+
 
 
 if __name__ == '__main__':
